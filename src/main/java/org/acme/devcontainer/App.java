b892ed1baecad0c26d9e1f9c927b292247f5ad93
Andrea Chiumenti <achiumenti@red.software.systems>
2026-06-18 09:54:29 +0200

    first commit

---
+++ src/main/java/org/acme/devcontainer/App.java
@@ -0,0 +1,18 @@
+package org.acme.devcontainer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( App.printHello() );
+    }
+
+    public static String printHello()
+    {
+        return "Hello World!";
+    }
+}
